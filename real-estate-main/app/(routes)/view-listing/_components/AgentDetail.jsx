9b4e9df990a3aad52253dd8bd88425135f337c10
Jonathan Kong <jonathankong1996@gmail.com>
2024-05-22 22:52:39 +0800

    feat: add auto-emailer when `send message` btn is pressed

--- real-estate-main/app/(routes)/view-listing/_components/AgentDetail.jsx
+++ real-estate-main/app/(routes)/view-listing/_components/AgentDetail.jsx
@@ -1,26 +1,41 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React from 'react';
 
-function AgentDetail({listingDetail}) {
+function AgentDetail({ listingDetail }) {
+  const handleSendMessage = () => {
+    const recipient = listingDetail?.createdBy || '';
+    const subject = 'Interested in Buying Property';
+    const body =
+      'Hi, I am interested in buying the property. How can we go about making this deal?';
+    const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
+      subject,
+    )}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+  };
+
   return (
-    <div className='flex gap-5 items-center justify-between 
-    p-5 rounded-lg shadow-md border my-6'>
-        <div className='flex items-center gap-6'>
-            <Image src={listingDetail?.profileImage}
-            alt='profileImage'
-            width={60}
-            height={60}
-            className='rounded-full'
-            />
-            <div>
-                <h2 className='text-lg font-bold'>{listingDetail?.fullName}</h2>
-                <h2 className='text-gray-500'>{listingDetail?.createdBy}</h2>
-            </div>
+    <div
+      className="flex gap-5 items-center justify-between 
+    p-5 rounded-lg shadow-md border my-6"
+    >
+      <div className="flex items-center gap-6">
+        <Image
+          src={listingDetail?.profileImage}
+          alt="profileImage"
+          width={60}
+          height={60}
+          className="rounded-full"
+        />
+        <div>
+          <h2 className="text-lg font-bold">{listingDetail?.fullName}</h2>
+          <h2 className="text-gray-500">{listingDetail?.createdBy}</h2>
         </div>
-        <Button>Send Message</Button>
+      </div>
+      <Button onClick={handleSendMessage}>Send Message</Button>
     </div>
-  )
+  );
 }
 
-export default AgentDetail
+export default AgentDetail;
